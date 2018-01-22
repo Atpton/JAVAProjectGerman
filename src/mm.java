202f932bf33822908c3389da239376dbdd91344e
Atpton <s5704062636047@email.kmutnb.ac.th>
2018-01-22 11:07:31 +0100

    tonbrach

--- src/mm.java
+++ src/mm.java
@@ -1,12 +1,10 @@
-ffffff
+import java.util.Random;
 
-
-boom2
-
-
-
-boom
-
-foam
-nb
-
+public class mm {
+	public static void main(String[] args) {  
+		
+		Random rand = new Random();
+		for(int i=0;i<20;i++) 
+			if(20>rand.nextInt()) System.out.println(1);
+	}
+}
